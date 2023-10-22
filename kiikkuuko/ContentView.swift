71943cf958a2f205338a1b2bb34c0ad9c7f37812
Wolf Wikgren <wolf.wikgren@gmail.com>
2023-10-22 21:36:53 +0300

    Initial Commit

---
+++ kiikkuuko/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kiikkuuko
+//
+//  Created by Wolf Wikgren on 22.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kiikkuuko/kiikkuukoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiikkuukoApp.swift
+//  kiikkuuko
+//
+//  Created by Wolf Wikgren on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct kiikkuukoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
